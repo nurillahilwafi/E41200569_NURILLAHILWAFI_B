4d09207ceee6307e03d9d032da4277de92a57fad
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-16 17:01:32 +0700

    Create Person.java

---
+++ minggu 2/tugas/person/src/person/Person.java
@@ -0,0 +1,16 @@
+
+package person;
+
+public class Person {
+
+    public static void main(String[] args) {
+        String fName = "Lisa Palombo";
+        int stuId = 123456789;
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name = " + fName);
+        System.out.println("Student ID = " + stuId);
+        System.out.println("Student Status = " + stuStatus);
+    }
+    
+}
