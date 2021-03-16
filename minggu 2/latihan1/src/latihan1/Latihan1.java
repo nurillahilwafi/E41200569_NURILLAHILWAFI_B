231013269788af76114c0def6fe39001c8069a87
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-16 15:32:28 +0700

    Create Latihan1.java

---
+++ minggu 2/latihan1/src/latihan1/Latihan1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello ");
+        System.out.println("world !");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.print("Hello ");
+        System.out.print("world !");
+    }
+    
+}
