4e6f9d801dfc97892e631ae6104dab46b7a8058c
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-16 20:24:15 +0700

    Create Tugas4.java

---
+++ minggu 2/tugas/tugas4/src/tugas4/Tugas4.java
@@ -0,0 +1,18 @@
+
+package tugas4;
+
+public class Tugas4 {
+
+    public static void main(String[] args) {
+        String s1 = "ABC";
+        String s2 = "DEF";
+        String s3 = "AB" + "C";
+        
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.equals(s3));
+        System.out.println(s3.equals(s1));
+        System.out.println(s2.compareTo(s3));
+        System.out.println(s3.equals(s1));
+    }
+    
+}
