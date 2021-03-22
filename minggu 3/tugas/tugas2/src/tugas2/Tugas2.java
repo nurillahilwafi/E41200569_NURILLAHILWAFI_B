97975844bcf4af930d99094d8ab0b2df2327ebef
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-23 02:46:40 +0700

    Create Tugas2.java

---
+++ minggu 3/tugas/tugas2/src/tugas2/Tugas2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+/**
+ *
+ * @author User
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int [] angka = {3,5,4,1,9,7,4};
+        
+        for (int i = 0; i<angka.length; i++){
+            System.out.println(angka[i]);
+        }
+    }
+    
+}
