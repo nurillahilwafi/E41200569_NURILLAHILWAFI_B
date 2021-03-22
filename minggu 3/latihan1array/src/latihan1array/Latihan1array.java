f6b9b39c44af95c6b480e20d9001b79db98344e2
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-22 21:27:01 +0700

    Create Latihan1array.java

---
+++ minggu 3/latihan1array/src/latihan1array/Latihan1array.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1array;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan1array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] number = {1,2,3,4,5,6};
+        
+        System.out.println("jumlah array adalah = " + number.length);
+    }
+    
+}
