0bd20b94d833b0b813226573ac427721149cc0c2
nurillahilwafi <nurilwafi17@gmail.com>
2021-03-23 01:21:00 +0700

    Create Tugas1.java

---
+++ minggu 3/tugas/tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,14 @@
+
+package tugas1;
+
+public class Tugas1 {
+
+    public static void main(String[] args) {
+        String [] nama = {"nuril", "lahil", "wafi"};
+        for (int w=0; w<nama.length; w++){
+            System.out.print(nama[w]);
+            System.out.print(" ");
+        }
+    }
+    
+}
