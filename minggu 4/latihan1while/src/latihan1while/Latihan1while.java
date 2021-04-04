9e8b34a29152281c8536c28ecc91881640cc5649
nurillahilwafi <nurilwafi17@gmail.com>
2021-04-05 01:51:18 +0700

    Create Latihan1while.java

---
+++ minggu 4/latihan1while/src/latihan1while/Latihan1while.java
@@ -0,0 +1,15 @@
+
+package latihan1while;
+
+public class Latihan1while {
+
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10) {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        }
+    }
+    
+}
