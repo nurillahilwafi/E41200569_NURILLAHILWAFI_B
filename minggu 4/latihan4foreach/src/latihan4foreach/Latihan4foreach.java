04c0523d6b8858c56cb92fb49a4b0edc9955464a
nurillahilwafi <nurilwafi17@gmail.com>
2021-04-05 01:58:36 +0700

    Create Latihan4foreach.java

---
+++ minggu 4/latihan4foreach/src/latihan4foreach/Latihan4foreach.java
@@ -0,0 +1,13 @@
+
+package latihan4foreach;
+
+public class Latihan4foreach {
+
+    public static void main(String[] args) {
+        int angka[] = {3,1,42,24,12};
+        for ( int x : angka ){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
