12204438cc569f88dbfbbb322e7b0ab32664d8b0
nurillahilwafi <nurilwafi17@gmail.com>
2021-04-05 01:51:33 +0700

    Create Latihan2dowhile.java

---
+++ minggu 4/latihan2dowhile/src/latihan2dowhile/Latihan2dowhile.java
@@ -0,0 +1,15 @@
+
+package latihan2dowhile;
+
+public class Latihan2dowhile {
+
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        }while ( i <= 10);
+    }
+    
+}
