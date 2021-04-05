caca45a20d669f6bf3828c7999512168be7e0862
nurillahilwafi <nurilwafi17@gmail.com>
2021-04-05 09:35:01 +0700

    Create Tugas1.java

---
+++ minggu 4/tugas/tugas1/src/tugas1/Tugas1.java
@@ -0,0 +1,15 @@
+
+package tugas1;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 20; i++){
+            if (i%2 == 0){
+                System.out.print(i);
+            }else {
+                System.out.print(" ");
+            }
+        }
+    }
+    
+}
