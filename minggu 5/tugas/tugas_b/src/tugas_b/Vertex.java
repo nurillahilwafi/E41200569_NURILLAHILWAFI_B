f227f85773ab9af81ca2fb6a438d49281b475219
nurillahilwafi <nurilwafi17@gmail.com>
2021-04-13 22:57:37 +0700

    Create Vertex.java

---
+++ minggu 5/tugas/tugas_b/src/tugas_b/Vertex.java
@@ -0,0 +1,11 @@
+package tugas_b;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char lab) {
+        this.label = lab; 
+        wasVisited = false;
+    }
+}
